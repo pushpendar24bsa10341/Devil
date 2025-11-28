539e543ab8fa73ae68bda86f7164c2690fd76190
pushpendar24bsa10341 <pushpendar.24bsa10341@vitbhopal.ac.in>
2025-11-28 13:15:33 +0530

    Update User.java

--- src/ecosmartwaste/User.java
+++ src/ecosmartwaste/User.java
@@ -1,1 +1,25 @@
+package ecosmartwaste;
 
+public class User {
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public boolean authenticate(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+}
